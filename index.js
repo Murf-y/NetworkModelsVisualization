48e2e45d9f4dbecb95f7141084bffea31bbe11ba
Murf-y <charbelfayad64@gmail.com>
2023-10-14 19:45:01 +0300

    feat: refactor into OOP in javascript XD

--- index.js
+++ index.js
@@ -1,174 +1,239 @@
-// get initial N and p values
-const nInput = document.getElementById("n");
-const pInput = document.getElementById("p");
-const mInput = document.getElementById("m");
-const m0Input = document.getElementById("m0");
-
-// create enum for graph types
 const GraphType = {
-  ERDOS_RENYI: "erdos_renyi",
-  BARABASI_ALBERT: "barabasi_albert",
+  ERDOS_RENYI: "erdos-renyi",
+  BARABASI_ALBERT: "barabasi-albert",
 };
 
-let currentGraphType = GraphType.ERDOS_RENYI;
-updateParametersSection();
+class Visualizer {
+  constructor(graphType) {
+    this.graphType = graphType;
+    // for the layout
+    this.sideBarManager = new SideBarManager(graphType, (graphType) => {
+      this.updateGraphType(graphType);
+    });
 
-// on change of graph type, update the graph
-const graphTypeSelect = document.getElementById("model-select");
-graphTypeSelect.addEventListener("change", (event) => {
-  const newGraphType = event.target.value;
-  if (newGraphType !== currentGraphType) {
-    currentGraphType = newGraphType;
-    updateParametersSection();
-    updateGraph();
+    cytoscape.use(cytoscapeCola);
+    this.initializeGraph();
+    this.sideBarManager.updateMetrics(this.cy);
   }
-});
 
-function updateParametersSection() {
-  if (currentGraphType === GraphType.ERDOS_RENYI) {
-    document.getElementById("erdos-renyi-parameters").style.display = "flex";
-    document.getElementById("barabasi-albert-parameters").style.display =
-      "none";
-  } else {
-    document.getElementById("erdos-renyi-parameters").style.display = "none";
-    document.getElementById("barabasi-albert-parameters").style.display =
-      "flex";
+  updateGraphType(graphType) {
+    this.graphType = graphType;
+    // delete the old cy instance
+    this.cy.destroy();
+    this.initializeGraph();
+    this.sideBarManager.changeGraphType(graphType);
+    this.sideBarManager.updateMetrics(this.cy);
   }
-}
 
-function createGraph() {
-  if (currentGraphType === GraphType.ERDOS_RENYI) {
-    return createErdosRenyiGraph();
-  } else {
-    return createBarabasiAlbertGraph();
+  initializeGraph() {
+    this.cy = cytoscape({
+      container: document.getElementById("cy"),
+      elements: this.creataGraph(),
+
+      style: [
+        // the stylesheet for the graph
+        {
+          selector: "node",
+          style: {
+            "background-color": "#7888ff",
+            label: "data(id)",
+            "font-family": "Roboto",
+            "font-size": 6,
+          },
+        },
+
+        {
+          selector: "edge",
+          style: {
+            width: 2,
+            "line-color": "#bfe0ff",
+          },
+        },
+      ],
+
+      layout: {
+        name: "cola",
+        nodeSpacing: 20,
+      },
+
+      minZoom: 0.4,
+      maxZoom: 2,
+    });
   }
-}
 
-function updateMetrics() {
-  const nodes = cy.nodes().length;
-  const edges = cy.edges().length;
-  const avgDegree = (2 * edges) / nodes;
-  const maxDegree = cy.nodes().reduce((acc, node) => {
-    const degree = node.degree();
-    return degree > acc ? degree : acc;
-  }, 0);
-
-  document.getElementById("nodes-value").innerText = nodes;
-  document.getElementById("edges-value").innerText = edges;
-  document.getElementById("avg-degree-value").innerText = avgDegree.toFixed(2);
-  document.getElementById("max-degree-value").innerText = maxDegree;
-}
-
-function createErdosRenyiGraph() {
-  const N = parseInt(nInput.value);
-  const p = parseFloat(pInput.value);
-  const edges = [];
-
-  for (let i = 0; i < N; i++) {
-    for (let j = i + 1; j < N; j++) {
-      if (Math.random() < p) {
-        edges.push({ group: "edges", data: { source: i, target: j } });
-      }
+  creataGraph() {
+    if (this.graphType === GraphType.ERDOS_RENYI) {
+      return this.createErdosRenyiGraph();
+    } else {
+      return this.createBarabasiAlbertGraph();
     }
   }
 
-  const nodes = Array.from({ length: N }, (_, i) => ({
-    group: "nodes",
-    data: { id: i },
-  }));
+  createErdosRenyiGraph() {
+    const N = this.sideBarManager.getN();
+    const p = this.sideBarManager.getP();
 
-  const elements = nodes.concat(edges);
+    console.log("Creating graph with N = ", N, " and p = ", p);
+    const edges = [];
 
-  return elements;
-}
+    const nodes = Array.from({ length: N }, (_, i) => ({
+      group: "nodes",
+      data: { id: i },
+    }));
 
-function createBarabasiAlbertGraph() {
-  const N = parseInt(nInput.value);
-  const m0 = parseInt(m0Input.value);
-  const m = parseInt(mInput.value);
-
-  const nodes = Array.from({ length: m0 }, (_, i) => ({
-    group: "nodes",
-    data: { id: i },
-  }));
-
-  const edges = [];
-
-  for (let i = 0; i < m0; i++) {
-    for (let j = i + 1; j < m0; j++) {
-      edges.push({ group: "edges", data: { source: i, target: j } });
-    }
-  }
-
-  for (let i = m0; i < N; i++) {
-    const newEdges = [];
-    const degrees = new Map();
-
-    for (let j = 0; j < i; j++) {
-      const degree = cy.$(`node[id="${j}"]`).degree();
-      degrees.set(j, degree);
-    }
-
-    const sum = Array.from(degrees.values()).reduce((a, b) => a + b, 0);
-
-    for (let j = 0; j < m; j++) {
-      const r = Math.random();
-      let acc = 0;
-
-      for (let k = 0; k < i; k++) {
-        acc += degrees.get(k) / sum;
-
-        if (r < acc) {
-          newEdges.push({ group: "edges", data: { source: i, target: k } });
-          break;
+    for (let i = 0; i < N; i++) {
+      for (let j = i + 1; j < N; j++) {
+        if (Math.random() <= p) {
+          edges.push({ group: "edges", data: { source: i, target: j } });
         }
       }
     }
 
-    nodes.push({ group: "nodes", data: { id: i } });
-    edges.push(...newEdges);
+    const elements = nodes.concat(edges);
+
+    return elements;
   }
 
-  const elements = nodes.concat(edges);
+  createBarabasiAlbertGraph() {
+    const N = this.sideBarManager.getN();
+    const m0 = this.sideBarManager.getM0();
+    const m = this.sideBarManager.getM();
 
-  return elements;
+    console.log(
+      "Creating graph with N = ",
+      N,
+      " and m0 = ",
+      m0,
+      " and m = ",
+      m
+    );
+
+    const nodes = Array.from({ length: m0 }, (_, i) => ({
+      group: "nodes",
+      data: { id: i },
+    }));
+
+    const edges = [];
+    const degrees = new Array(N).fill(0); // Initialize the degree array.
+
+    for (let i = 0; i < m0; i++) {
+      for (let j = i + 1; j < m0; j++) {
+        edges.push({ group: "edges", data: { source: i, target: j } });
+        degrees[i] += 1; // Update the degree for node i.
+        degrees[j] += 1; // Update the degree for node j.
+      }
+    }
+
+    for (let i = m0; i < N; i++) {
+      const newEdges = [];
+      const sum = degrees.reduce((a, b) => a + b, 0);
+
+      for (let j = 0; j < m; j++) {
+        const r = Math.random();
+        let acc = 0;
+
+        for (let k = 0; k < i; k++) {
+          acc += degrees[k] / sum;
+
+          if (r < acc) {
+            newEdges.push({ group: "edges", data: { source: i, target: k } });
+            degrees[i] += 1;
+            degrees[k] += 1;
+            break;
+          }
+        }
+      }
+
+      nodes.push({ group: "nodes", data: { id: i } });
+      edges.push(...newEdges);
+    }
+
+    const elements = nodes.concat(edges);
+
+    return elements;
+  }
 }
 
-cytoscape.use(cytoscapeCola);
-const cy = cytoscape({
-  container: document.getElementById("cy"), // container to render in
+class SideBarManager {
+  constructor(graphType, updateParentCallback) {
+    this.graphType = graphType;
+    this.updateParentCallback = updateParentCallback;
+    this.nInput = document.getElementById("n");
+    this.pInput = document.getElementById("p");
+    this.mInput = document.getElementById("m");
+    this.m0Input = document.getElementById("m0");
+    this.graphTypeSelect = document.getElementById("model-select");
 
-  elements: createGraph(),
+    this.nInput.addEventListener("change", () => {
+      this.updateParentCallback(this.graphType);
+    });
 
-  style: [
-    // the stylesheet for the graph
-    {
-      selector: "node",
-      style: {
-        "background-color": "#7888ff",
-        label: "data(id)",
-        "font-family": "Roboto",
-        "font-size": 8,
-      },
-    },
+    this.graphTypeSelect.addEventListener("change", (event) => {
+      const newGraphType = event.target.value;
+      if (newGraphType !== this.graphType) {
+        this.graphType = newGraphType;
+        this.updateParametersSection();
+        this.updateParentCallback(this.graphType);
+      }
+    });
 
-    {
-      selector: "edge",
-      style: {
-        width: 2,
-        "line-color": "#bfe0ff",
-      },
-    },
-  ],
+    this.updateParametersSection();
+  }
 
-  layout: {
-    name: "cola",
-    nodeSpacing: 20,
-  },
+  changeGraphType(graphType) {
+    // Requires the caller to update the metrics
+    this.graphType = graphType;
+    this.updateParametersSection();
+  }
 
-  minZoom: 0.4,
-  maxZoom: 2,
-});
+  updateParametersSection() {
+    if (this.graphType === GraphType.ERDOS_RENYI) {
+      document.getElementById("erdos-renyi-parameters").style.display = "flex";
+      document.getElementById("barabasi-albert-parameters").style.display =
+        "none";
+    } else {
+      document.getElementById("erdos-renyi-parameters").style.display = "none";
+      document.getElementById("barabasi-albert-parameters").style.display =
+        "flex";
+    }
+  }
+
+  updateMetrics(cyInstance) {
+    const nodes = cyInstance.nodes().length;
+    const edges = cyInstance.edges().length;
+    const avgDegree = (2 * edges) / nodes;
+    let maxDegree = 0;
+
+    for (let i = 0; i < nodes; i++) {
+      const degree = cyInstance.$(`node[id="${i}"]`).degree();
+      if (degree > maxDegree) {
+        maxDegree = degree;
+      }
+    }
+
+    document.getElementById("nodes-value").innerText = nodes;
+    document.getElementById("edges-value").innerText = edges;
+    document.getElementById("avg-degree-value").innerText =
+      avgDegree.toFixed(2);
+    document.getElementById("max-degree-value").innerText = maxDegree;
+  }
+
+  getN() {
+    return parseInt(this.nInput.value);
+  }
+
+  getP() {
+    return parseFloat(this.pInput.value);
+  }
+
+  getM() {
+    return parseInt(this.mInput.value);
+  }
+
+  getM0() {
+    return parseInt(this.m0Input.value);
+  }
+}
 
 function updateGraphNodes() {
   const oldN = cy.nodes().length;
@@ -278,9 +343,4 @@ function updateGraphEdgesBasedOnP() {
   updateMetrics();
 }
 
-nInput.addEventListener("input", updateGraphNodes);
-pInput.addEventListener("input", updateGraphEdgesBasedOnP);
-mInput.addEventListener("input", updateGraphNodes);
-m0Input.addEventListener("input", updateGraphNodes);
-
-updateMetrics();
+const visualizer = new Visualizer(GraphType.ERDOS_RENYI);
